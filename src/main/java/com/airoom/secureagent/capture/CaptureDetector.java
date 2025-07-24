f6ce6c82d123fc1b1fce21724be16b9cea449ae3
Jihyuck <wlgur4772@gmail.com>
2025-07-24 11:27:31 +0900

    Feat: 캡쳐 감지 기능 (CaptureDetector)

---
+++ src/main/java/com/airoom/secureagent/capture/CaptureDetector.java
@@ -0,0 +1,44 @@
+package com.airoom.secureagent.capture;
+
+import com.airoom.secureagent.log.HttpLogger;
+import com.airoom.secureagent.log.LogManager;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+public class CaptureDetector {
+
+    // PrintScreen 감지
+
+    public interface User32 extends Library {
+        User32 INSTANCE = Native.load("user32", User32.class);
+        short GetAsyncKeyState(int keyCode);
+    }
+
+    public static void startCaptureWatch() {
+        new Thread(() -> {
+            System.out.println("[SecureAgent] 캡처 감지 스레드 시작됨");
+            while (true) {
+                try {
+                    // 1. PrintScreen 키 감지
+                    if (User32.INSTANCE.GetAsyncKeyState(0x2C) != 0) {
+                        String msg = "[감지] 사용자가 PrintScreen 키를 눌렀습니다!";
+                        System.out.println(msg);
+
+                        LogManager.writeLog(msg);
+                        HttpLogger.sendLog(msg);
+
+                        Thread.sleep(1000); // 중복 감지 방지
+                    }
+
+                    // 2. 캡처 도구 실행 감지
+                    ProcessMonitor.detect();
+
+                    Thread.sleep(200); // 감지 주기 (0.2초)
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
